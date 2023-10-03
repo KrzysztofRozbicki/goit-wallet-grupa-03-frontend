f06f4c99736c402c1d1be655a25a6e40b268e134
Krzysztof R <plkrozbicki@gmail.com>
2023-10-03 10:40:01 +0100

    Prepare icons, modern-normalize, vars and fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
+import wallet from '../assets/icons/wallet.svg';
+import banner from '../assets/icons/banner.svg';
+
 function App() {
   return (
     <>
       <h1>This is the Quick Buck Devs</h1>
+      <p> bellow are the test show of icons</p>
+      <img src={wallet} />
+      <img src={banner} />
     </>
   );
 }
