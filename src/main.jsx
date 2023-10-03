d026e5546d433e8e14836be40e974261a2b3705b
Krzysztof R <plkrozbicki@gmail.com>
2023-10-03 18:59:13 +0100

    import fonts to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import './stylesheet/fonts.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
