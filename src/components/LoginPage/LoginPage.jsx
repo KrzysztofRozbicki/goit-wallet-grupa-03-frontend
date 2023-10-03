0301a2940e9235ba62d4c4e02724bd5deb443181
Chrapson <marcin.chraplewski@gmail.com>
2023-10-04 01:05:23 +0200

    chore: add login form tablet and desktop view

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -28,13 +28,24 @@
 
 import LoginForm from '../LoginForm/LoginForm';
 import css from './LoginPage.module.css';
-import { bannerSVG } from '../../assets/icons/banner.svg';
+import bannerSVG from '../../assets/icons/banner.svg';
+import blob1 from '../../assets/icons/blob-1.svg';
+import blob2 from '../../assets/icons/blob-2.svg';
 
 const LoginPage = () => {
   return (
     <>
-      {/* <img src={bannerSVG}/> */}
-      <LoginForm />
+      <div className={css.wrapper}>
+        <div className={css.header}>
+          <img className={css.blob2} src={blob2} alt="blob" />
+          <img className={css.blob1} src={blob1} alt="blob" />
+          <img className={css.banner} src={bannerSVG} alt="banner" />
+          <h2 className={css.title}>Finance App</h2>
+        </div>
+        <div className={css.container}>
+          <LoginForm />
+        </div>
+      </div>
     </>
   );
 };
