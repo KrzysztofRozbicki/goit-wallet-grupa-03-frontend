98b6bcb12fe1185be8fce042ca97ef21aab71674
Krzysztof R <plkrozbicki@gmail.com>
2023-10-04 18:54:28 +0100

    position bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,38 +9,42 @@ import TestComponentRedux from './TestComponentRedux/TestComponentRedux';
 import LoginPage from './LoginPage/LoginPage';
 import RegistrationPage from './RegistrationPage/RegistrationPage';
 
-// const router = createBrowserRouter([
-//   {
-//     path: 'goit-wallet-grupa-03-frontend/login',
-//     element: <LoginPage />,
-//   },
-//   {
-//     path: 'goit-wallet-grupa-03-frontend/register',
-//     element: <RegistrationPage />,
-//   },
+const router = createBrowserRouter([
+  {
+    path: 'goit-wallet-grupa-03-frontend/login',
+    element: <LoginPage />,
+  },
+  {
+    path: 'goit-wallet-grupa-03-frontend/register',
+    element: <RegistrationPage />,
+  },
 
-//   {
-//     path: 'goit-wallet-grupa-03-frontend/',
-//     element: <DashboardPage />,
-//     children: [
-//       {
-//         path: '',
-//         element: <HomeTab />,
-//       },
-//       {
-//         path: 'chart',
-//         element: <Chart />,
-//       },
-//       {
-//         path: 'diagram',
-//         element: <DiagramTab />,
-//       },
-//     ],
-//   },
-// ]);
+  {
+    path: 'goit-wallet-grupa-03-frontend/',
+    element: <DashboardPage />,
+    children: [
+      {
+        path: '',
+        element: <HomeTab />,
+      },
+      {
+        path: 'chart',
+        element: <Chart />,
+      },
+      {
+        path: 'diagram',
+        element: <DiagramTab />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>{/* <RouterProvider router={router} /> */}</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
