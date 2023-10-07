7a61962250c89dd480acf3f38f9e199f5177f70b
Krzysztof R <plkrozbicki@gmail.com>
2023-10-07 09:49:41 +0100

    Update mock data to server

--- src/mock/transactions.js
+++ src/mock/transactions.js
@@ -2,156 +2,154 @@ export default [
   {
     id: 'sda123',
     isIncome: true,
-    owner: '12233', //john
+    user: '12233', //john
     date: '2023-10-02',
-    type: '+',
     category: 'Income',
     comment: 'salary',
-    amount: '30000',
+    amount: 30000,
   },
   {
     id: 'sda144',
     isIncome: false,
-    owner: '12233', //john
+    user: '12233', //john
     date: '2023-10-03',
-    type: '-',
     category: 'Self care',
     comment: 'spa visit',
-    amount: '200',
+    amount: 200,
   },
   {
     id: 'sdawe1543',
-    owner: '12233', //john
+    isIncome: false,
+    user: '12233', //john
     date: '2023-10-07',
-    type: '-',
     category: 'Household products',
     comment: 'vacuum cleaner',
-    amount: '1500',
+    amount: 1500,
   },
   {
     id: 'qwer43',
-    owner: '13324', //Anna
+    user: '13324', //Anna
+    isIncome: true,
     date: '2023-10-02',
-    type: '+',
     category: 'Income',
     comment: 'Salary',
-    amount: '45000',
+    amount: 45000,
   },
   {
     id: 'zxas3f123',
-    owner: '13324', //Anna
+    isIncome: false,
+    user: '13324', //Anna
     date: '2023-10-11',
-    type: '-',
     category: 'Entertainment',
     comment: '',
-    amount: '500',
+    amount: 500,
   },
   {
     id: 'qwe1r4543',
-    owner: '15478', //fluffy
+    isIncome: true,
+    user: '15478', //fluffy
     date: '2023-10-21',
-    type: '+',
     category: 'Income',
     comment: '',
-    amount: '100000',
+    amount: 100000,
   },
   {
     id: 'qfasf1854as474',
-    owner: '15478', //fluffy
+    isIncome: false,
+    user: '15478', //fluffy
     date: '2023-10-01',
-    type: '-',
     category: 'Main expenses',
     comment: 'toys',
-    amount: '1000',
+    amount: 1000,
   },
   {
     id: 'qwoiy1tq21w3124',
-    owner: '15478', //fluffy
+    isIncome: false,
+    user: '15478', //fluffy
     date: '2023-10-15',
-    type: '-',
     category: 'Education',
     comment: 'How to rule the world but Cat Meowman',
-    amount: '500',
+    amount: 500,
   },
   {
     id: 'qw1er43',
-    owner: '13324', //Anna
+    isIncome: false,
+    user: '13324', //Anna
     date: '2023-10-02',
-    type: '+',
     category: 'Income',
     comment: 'Salary',
-    amount: '45000',
+    amount: 45000,
   },
   {
     id: 'zxa1sf123',
-    owner: '13324', //Anna
+    isIncome: false,
+    user: '13324', //Anna
     date: '2023-10-11',
-    type: '-',
     category: 'Entertainment',
     comment: '',
-    amount: '500',
+    amount: 500,
   },
   {
     id: 'qwer142543',
-    owner: '15478', //fluffy
+    isIncome: true,
+    user: '15478', //fluffy
     date: '2023-10-21',
-    type: '+',
     category: 'Income',
     comment: '',
-    amount: '100000',
+    amount: 100000,
   },
   {
     id: 'qfasf197as474',
-    owner: '15478', //fluffy
+    isIncome: false,
+    user: '15478', //fluffy
     date: '2023-10-01',
-    type: '-',
     category: 'Main expenses',
     comment: 'toys',
-    amount: '1000',
+    amount: 1000,
   },
   {
     id: 'qwoiy123211tqw3124',
-    owner: '15478', //fluffy
+    isIncome: false,
+    user: '15478', //fluffy
     date: '2023-10-15',
-    type: '-',
     category: 'Education',
-    comment: 'How to rule the world but Cat Meowman',
-    amount: '500',
+    comment: 'How to rule the world by Cat Meowman',
+    amount: 500,
   },
   {
     id: 'z1xa1sf123',
-    owner: '13324', //Anna
+    isIncome: false,
+    user: '13324', //Anna
     date: '2023-10-11',
-    type: '-',
     category: 'Entertainment',
     comment: '',
-    amount: '500',
+    amount: 500,
   },
   {
     id: 'q1wer142543',
-    owner: '15478', //fluffy
+    isIncome: true,
+    user: '15478', //fluffy
     date: '2023-10-21',
-    type: '+',
     category: 'Income',
     comment: '',
-    amount: '100000',
+    amount: 100000,
   },
   {
     id: 'q1fasf197as474',
-    owner: '15478', //fluffy
+    isIncome: false,
+    user: '15478', //fluffy
     date: '2023-10-01',
-    type: '-',
     category: 'Main expenses',
     comment: 'toys',
-    amount: '1000',
+    amount: 1000,
   },
   {
     id: 'q1woiy123211tqw3124',
-    owner: '15478', //fluffy
+    isIncome: false,
+    user: '15478', //fluffy
     date: '2023-10-15',
-    type: '-',
     category: 'Education',
     comment: 'How to rule the world but Cat Meowman',
-    amount: '500',
+    amount: 500,
   },
 ];
